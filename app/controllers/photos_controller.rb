269b61e94b2fd51062f45dc8a639b71a2c090bda
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-11-02 22:17:34 +0000

    4

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,14 @@
 class PhotosController < ApplicationController
 
   def index
-  
+  matching_photos = Photo.all.order({:created_at => :desc})
+  @list_of_photos = matching_photos
   render({ :template => "photo_templates/index.html.erb"})
   end
 
   def show
-  
+  url_photoid = params.fetch("path_photoid")
+  @the_photo = Photo.where({ :id => url_photoid }).first
   render({ :template => "photo_templates/show.html.erb"})
   end
 
